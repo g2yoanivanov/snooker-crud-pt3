67193b96b6fe50cff10a8fe00799fc5fc0932d73
Victor147 <viktorivanov_e18@schoolmath.eu>
2022-06-10 22:32:50 +0300

    Fixed DBContext Class

--- DataLayer/SnookerCRUDDbContext.cs
+++ DataLayer/SnookerCRUDDbContext.cs
@@ -11,7 +11,6 @@ namespace DataLayer
     public class SnookerCRUDDbContext : DbContext
     {
         public SnookerCRUDDbContext()
-<<<<<<< HEAD
         {
 
         }
@@ -20,27 +19,13 @@ namespace DataLayer
         {
 
         }   
-=======
-        {
-
-        }
-
-        public SnookerCRUDDbContext(DbContextOptions options) : base(options)
-        {
-
-        }
->>>>>>> 930b273d2c98a64c5c3c6a23a778d25ac414e5cc
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
                 //Yoan's connection string
-<<<<<<< HEAD
                 //optionsBuilder.UseSqlServer(@"Server=DESKTOP-HB55C5B;Database=SnookerCRUDDb;Trusted_Connection=True;");
-=======
-                optionsBuilder.UseSqlServer(@"Server=DESKTOP-HB55C5B;Database=SnookerCRUDDb;Trusted_Connection=True;");
->>>>>>> 930b273d2c98a64c5c3c6a23a778d25ac414e5cc
                 
                 //Victor's connection string
                 //optionsBuilder.UseSqlServer(@"Server=DESKTOP-54NFRM2\SQLEXPRESS;Database=SnookerCRUDDb;Trusted_Connection=True;");
