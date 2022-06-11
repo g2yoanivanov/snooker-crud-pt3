966d92eeb9287a34fd069e3b198c82a2e22b5403
Yoan Ivanov <yoan_ivanov1@yahoo.com>
2022-06-11 09:06:30 +0300

    Finished Testing Layer

---
+++ TestingLayer/PlayerContextUnitTest.cs
@@ -0,0 +1,86 @@
+﻿using NUnit.Framework;
+using BusinessLayer;
+using DataLayer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+
+namespace TestingLayer
+{
+    public class PlayerContextUnitTest
+    {
+        private SnookerCRUDDbContext dbContext;
+        private PlayerContext playerContext;
+        private CountryContext countryContext;
+        DbContextOptionsBuilder builder;
+
+        [SetUp]
+        public void Setup()
+        {
+            builder = new DbContextOptionsBuilder();
+            builder.UseInMemoryDatabase(Guid.NewGuid().ToString());
+
+            dbContext = new SnookerCRUDDbContext(builder.Options);
+            playerContext = new PlayerContext(dbContext);
+
+        }
+
+        [Test]
+        public void TestCreatePlayer()
+        {
+            int playersBefore = playerContext.ReadAll().Count();
+
+            Country country = countryContext.Read(1);
+            
+            playerContext.Create(new Player("Victor", "Ivanov", 18, country, 127, 1));
+
+            int playersAfter = playerContext.ReadAll().Count();
+
+            Assert.IsTrue(playersBefore != playersAfter);
+        }
+
+        [Test]
+        public void TestReadPlayer()
+        {
+            Country country = countryContext.Read(1);
+            playerContext.Create(new Player("Yoan", "Ivanov", 17, country, 0, 0));
+
+            Player player = playerContext.Read(1);
+
+            Assert.That(player != null, "There is no record with id 1");
+        }
+
+        [Test]
+        public void TestUpdatePlayer()
+        {
+            Country country = countryContext.Read(1);
+            playerContext.Create(new Player("Yoan", "Ivanov", 17, country, 0, 0));
+
+            Player player = playerContext.Read(1);
+
+            player.Age = 18;
+            player.MatchesPlayed = 1;
+
+            playerContext.Update(player);
+            Player player1 = playerContext.Read(1);
+
+            Assert.IsTrue(player1.Age == 18 && player1.MatchesPlayed == 1, "Player Update() does not change the age and the played matches");
+        }
+
+        [Test]
+        public void TestDeletePlayer()
+        {
+            Country country = countryContext.Read(1);
+            playerContext.Create(new Player("Iztrit", "Iztritko", 21, country, 0, 0));
+
+            int playersBeforeDelete = playerContext.ReadAll().Count();
+
+            playerContext.Delete(1);
+
+            int playersAfterDelete = playerContext.ReadAll().Count();
+
+            Assert.AreNotEqual(playersBeforeDelete, playersAfterDelete);
+        }
+    }
+}
---
+++ TestingLayer/TournamentContextUnitTest.cs
@@ -0,0 +1,78 @@
+﻿using NUnit.Framework;
+using BusinessLayer;
+using DataLayer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+
+namespace TestingLayer
+{
+    public class TournamentContextUnitTest
+    {
+        private SnookerCRUDDbContext dbContext;
+        private TournamentContext tournamentContext;
+
+        DbContextOptionsBuilder builder;
+
+        [SetUp]
+        public void Setup()
+        {
+            builder = new DbContextOptionsBuilder();
+            builder.UseInMemoryDatabase(Guid.NewGuid().ToString());
+
+            dbContext = new SnookerCRUDDbContext(builder.Options);
+            tournamentContext = new TournamentContext(dbContext);
+        }
+
+        [Test]
+        public void TestCreateTournament() 
+        {
+            int tournamentsBefore = tournamentContext.ReadAll().Count();
+
+            tournamentContext.Create(new Tournament("European Masters Qualifiers 1", "Leicester, England", 100000));
+
+            int tournamentsAfter = tournamentContext.ReadAll().Count();
+
+            Assert.IsTrue(tournamentsBefore != tournamentsAfter);
+        }
+
+        [Test]
+        public void TestReadTournament() 
+        {
+            tournamentContext.Create(new Tournament("European Masters", "Fuerth, Germany", 1000000));
+
+            Tournament tournament = tournamentContext.Read(1);
+
+            Assert.That(tournament != null, "There is no record with id 1");
+        }
+
+        [Test]
+        public void TestUpdateTournament() 
+        {
+            tournamentContext.Create(new Tournament("European Masters", "Fuerth, Germany", 1000000));
+
+            Tournament tournament = tournamentContext.Read(1);
+
+            tournament.PrizePool = 2000000;
+
+            tournamentContext.Update(tournament);
+
+            Tournament tournament1 = tournamentContext.Read(1);
+
+            Assert.IsTrue(tournament1.PrizePool == 2000000, "Tournament Update() does not change the prize pool");
+        }
+
+        [Test]
+        public void TestDeleteTournament() 
+        {
+            tournamentContext.Create(new Tournament("Wrong tournament", "Sofia, Bulgaria", 5000));
+
+            int tournamentsBeforeDelete = tournamentContext.ReadAll().Count();
+            tournamentContext.Delete(1);
+            int tournamentAfterDelete = tournamentContext.ReadAll().Count();
+
+            Assert.AreNotEqual(tournamentsBeforeDelete, tournamentAfterDelete);
+        }
+    }
+}
